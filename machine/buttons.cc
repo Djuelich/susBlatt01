0a38442edaf4c17561e30f7b1bfd8391ca6375ea
EduardRempel <erempel@uos.de>
2019-05-06 16:07:15 +0200

    add buttons function

--- machine/buttons.cc
+++ machine/buttons.cc
@@ -4,4 +4,17 @@
 Buttons Buttons::buttons; // // Singleton Instanz
 
 // Hier muesst ihr selbst Code ergaenzen
+void Buttons::enable(uint8_t button_bitmap){
+	P2OUT = P2OUT & ~button_bitmap;
+	P2DIR = P2DIR & ~button_bitmap;
+	P2REN = P2REN | button_bitmap;
+}
 
+
+uint8_t Buttons::read(){
+	return P2IN & 0b00010111;
+}
+
+bool Buttons::pressed(uint8_t button_bitmap){
+	return (read() == button_bitmap);
+}
